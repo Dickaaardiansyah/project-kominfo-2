d5e61a980713c9c1b427ddc5198664d771f1c629
Dickaardiansyah <dickaardiansyah994@gmail.com>
2025-07-31 09:12:22 +0700

    ra

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,22 +1,17 @@
 // D:\Projek Kominfo\project-kominfo-2\src\routes\AppRoutes.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-<<<<<<< HEAD
 import Toko from '../pages/Toko';
-=======
 import Scan from '../pages/Scan';
->>>>>>> b1685766d6427785b029fd70ebf3bc93852a6584
+
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
         <Route path="/toko" element={<Toko />} />
-=======
         <Route path="/scan" element={<Scan />} />
->>>>>>> b1685766d6427785b029fd70ebf3bc93852a6584
       </Routes>
     </BrowserRouter>
   );
